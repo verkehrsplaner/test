5e4b821637fd692e59e59ac963113ccb70ae0c3e
Maxi Krause <maxi.krause@me.com>
2014-05-07 13:41:25 +0200

    testgui erster versuch

--- src/verkehrsplaner_test/Verkehrsplaner_test.java
+++ src/verkehrsplaner_test/Verkehrsplaner_test.java
@@ -1,22 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package verkehrsplaner_test;
 
-/**
- *
- * @author Developer
- */
+import javax.swing.JFrame;
+
 public class Verkehrsplaner_test {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame f = new TestGUI();
+        f.setVisible(true);
     }
     
 }
