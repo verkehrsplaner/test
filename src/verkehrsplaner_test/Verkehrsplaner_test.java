03059eb79af2a91b8d99364b99d3a5777870cab2
Felix Maurer <felix.m@online.de>
2014-05-08 18:41:55 +0200

    Test

--- src/verkehrsplaner_test/Verkehrsplaner_test.java
+++ src/verkehrsplaner_test/Verkehrsplaner_test.java
@@ -9,6 +9,7 @@ public class Verkehrsplaner_test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Test!");
         JFrame f = new TestGUI();
         f.setVisible(true);
     }
