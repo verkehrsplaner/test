155ccf7bff4fd8152605390a0ef65b17910005b1
akfilm <akfilm@localhost>
2014-05-12 10:20:13 -0700

    Bunt

--- src/verkehrsplaner_test/TestPanel.java
+++ src/verkehrsplaner_test/TestPanel.java
@@ -6,6 +6,8 @@
 
 package verkehrsplaner_test;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -51,6 +53,17 @@ public class TestPanel extends javax.swing.JPanel {
     @Override
      public void paintComponent(Graphics g){
         Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(1));
+        g2d.fillRect(100, 100, 30, 30);
+        g2d.setColor(Color.YELLOW);
+        g2d.setStroke(new BasicStroke(3));
         g2d.drawRect(100, 100, 30, 30);
+        g2d.setColor(Color.RED);
+        g2d.setStroke(new BasicStroke(1));
+        g2d.fillRect(130, 100, 30, 30);
+        g2d.setColor(Color.YELLOW);
+        g2d.setStroke(new BasicStroke(3));
+        g2d.drawRect(130, 100, 30, 30);
     }
 }
