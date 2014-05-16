23a06a17b25eeb7cdfddc1099e1490ad8ee728d7
Felix Maurer <felix.m@online.de>
2014-05-16 16:37:03 +0200

    Sound check 1, 2, 3!

--- src/verkehrsplaner_test/Verkehrsplaner_test.java
+++ src/verkehrsplaner_test/Verkehrsplaner_test.java
@@ -14,6 +14,8 @@ public class Verkehrsplaner_test {
         
         SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
         System.out.println("Build Start: " + format.format(Calendar.getInstance().getTime()));
+        SoundCheck sound = new SoundCheck();
+        sound.start();
         System.out.println("Starte GUI..");
         JFrame f = new TestGUI();
         f.setVisible(true);
